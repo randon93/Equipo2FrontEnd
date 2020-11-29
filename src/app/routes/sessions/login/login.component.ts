71e13b96156cbbcd07e5861c0b3408199a42a3a9
TOLBAR <tolbarweb@gmail.com>
2020-11-28 22:20:59 -0500

    Ajuste login conectando con backend

--- src/app/routes/sessions/login/login.component.ts
+++ src/app/routes/sessions/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SettingsService, StartupService, TokenService } from '@core';
+import { LoginService } from '@shared/services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -15,15 +17,15 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private token: TokenService,
     private startup: StartupService,
-    private settings: SettingsService
+    private settings: SettingsService,
+    private loginService: LoginService,
+    private toastrService: ToastrService,
   ) {
     this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.pattern('ng-matero')]],
-      password: ['', [Validators.required, Validators.pattern('ng-matero')]],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
     });
   }
-
-  ngOnInit() {}
 
   get username() {
     return this.loginForm.get('username');
@@ -33,24 +35,34 @@ export class LoginComponent implements OnInit {
     return this.loginForm.get('password');
   }
 
+  ngOnInit() { }
+
   login() {
-    const { token, uid, username } = { token: 'ng-matero-token', uid: 1, username: 'ng-matero' };
-    // Set user info
-    this.settings.setUser({
-      id: uid,
-      name: 'Zongbin',
-      email: 'nzb329@163.com',
-      avatar: './assets/images/avatar.jpg',
-    });
-    // Set token info
-    this.token.set({ token, uid, username });
-    // Regain the initial data
-    this.startup.load().then(() => {
-      let url = this.token.referrer!.url || '/';
-      if (url.includes('/auth')) {
-        url = '/';
-      }
-      this.router.navigateByUrl(url);
+    let username = this.loginForm.get('username').value;
+    let clave = this.loginForm.get('password').value;
+    this.loginService.login(username, clave).subscribe((res: any) => {
+      let token: string = res.access_token;
+
+      // Set user info
+      this.settings.setUser({
+        id: 99999,
+        name: 'Biennnnz',
+        email: 'nzb329@163.com',
+        avatar: './assets/images/avatar.jpg',
+      });
+
+      // Set token info
+      this.token.set({ token, username });
+      // Regain the initial data
+      this.startup.load().then(() => {
+        let url = this.token.referrer!.url || '/';
+        if (url.includes('/auth')) {
+          url = '/';
+        }
+        this.router.navigateByUrl(url);
+      });
+    }, error => {
+      this.toastrService.error("Usuario y/o contraseña incorrecta");
     });
   }
 }
